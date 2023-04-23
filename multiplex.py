281c1dd07e57f1c8a309d30e9fac20c42a6a299b
Tommy <tommytran1999@gmail.com>
2023-04-22 21:01:34 -0400

    changed multiplex file into a supporting file (by turning mux into a class)

--- multiplex.py
+++ multiplex.py
@@ -3,32 +3,33 @@ import board
 from digitalio import DigitalInOut, Direction, Pull
 
 #Setting the pins as digital GPIO pins
-s = []
-s.append(DigitalInOut(board.GP5))
-s.append(DigitalInOut(board.GP4))
-s.append(DigitalInOut(board.GP3))
-s.append(DigitalInOut(board.GP2))
-SIG = DigitalInOut(board.GP1)
-EN  = DigitalInOut(board.GP6)
+class mux:
+    def __init__(self):
+        self.s = []
+        self.s.append(DigitalInOut(board.GP5))
+        self.s.append(DigitalInOut(board.GP4))
+        self.s.append(DigitalInOut(board.GP3))
+        self.s.append(DigitalInOut(board.GP2))
+        self.SIG = DigitalInOut(board.GP1)
+        self.EN  = DigitalInOut(board.GP6)
 
+        #Set the directions of the pins
+        self.SIG.direction = Direction.INPUT
+        self.EN.direction = Direction.OUTPUT
+        for i in range(0,4):
+            self.s[i].direction = Direction.INPUT
 
-#Set the directions of the pins
-SIG.direction = Direction.INPUT
-EN.direction = Direction.OUTPUT
-for i in range(0,4):
-    s[i].direction = Direction.INPUT
+    #change the input of the mux
+    def change_sig(self,num):
+        bin_num = format(num,'04b')
+        reverse_bin_num = bin_num[::-1]
 
-#change the input of the mux
-def change_sig(num):
-    bin_num = format(num,'04b')
-    reverse_bin_num = bin_num[::-1]
+        for i, bit in enumerate(reverse_bin_num):
+            if bit == '1':
+                self.s[3-i].pull = Pull.UP
 
-    for i, bit in enumerate(reverse_bin_num):
-        if bit == '1':
-            s[3-i].pull = Pull.UP
+    def enable_high(self):
+        self.EN.pull = Pull.UP
 
-def enable_high():
-    EN.pull = Pull.UP
-
-def enable_low():
-    EN.pull = Pull.DOWN 
+    def enable_low(self):
+        self.EN.pull = Pull.DOWN 
